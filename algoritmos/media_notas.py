b5ee0d2696f2a2502da3bf0c7ff423ff2c67214f
1giordani <giordanims@yahoo.com.br>
2026-09-08 16:10:39 -0300

    Add script to calculate average of three grades

---
+++ algoritmos/media_notas.py
@@ -0,0 +1,8 @@
+"""Calcula a média aritmética de três notas."""
+
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+
+media = (nota1 + nota2 + nota3) / 3
+print(f"Média: {media:.2f}")
